cd0817db989c99f03c32e7185bbd56fc1c0f4b21
stoically <29637501+stoically@users.noreply.github.com>
2017-11-29 04:08:35 +0100

    Speed up loading of content_script and make sure its loaded everywhere

--- tmpcontainer.js
+++ tmpcontainer.js
@@ -1,30 +1,32 @@
-document.body.addEventListener('mouseup', async function(event) {
-  // event valid?
-  if (typeof event !== 'object' || typeof event.target !== 'object') {
-    return;
-  }
-
-  // don't handle right mouse button
-  if (event.button === 2) {
-    return;
-  }
-
-  // only handle left mouse click if ctrl was clicked
-  if (event.button === 0 && !event.ctrlKey) {
-    return;
-  }
-
-  // check for a element with href
-  const aElement = event.target.closest('a');
-  if (aElement === null || typeof aElement !== 'object' || !aElement.href) {
-    return;
-  }
-
-  // tell background process to handle the clicked url
-  await browser.runtime.sendMessage({
-    linkClicked: {
-      href: aElement.href
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('mouseup', async (event) => {
+    // event valid?
+    if (typeof event !== 'object' || typeof event.target !== 'object') {
+      return;
     }
-  });
 
-}, false);
+    // don't handle right mouse button
+    if (event.button === 2) {
+      return;
+    }
+
+    // only handle left mouse click if ctrl was clicked
+    if (event.button === 0 && !event.ctrlKey) {
+      return;
+    }
+
+    // check for a element with href
+    const aElement = event.target.closest('a');
+    if (aElement === null || typeof aElement !== 'object' || !aElement.href) {
+      return;
+    }
+
+    // tell background process to handle the clicked url
+    await browser.runtime.sendMessage({
+      linkClicked: {
+        href: aElement.href
+      }
+    });
+
+  }, false);
+});
