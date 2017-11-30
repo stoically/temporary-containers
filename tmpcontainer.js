0142be310bac190ea3b45fa45f0a01257e609aef
stoically <29637501+stoically@users.noreply.github.com>
2017-11-30 20:01:43 +0100

    Reloading the user script doesnt fire DOM content loaded event, switch to document_end

--- tmpcontainer.js
+++ tmpcontainer.js
@@ -1,36 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.addEventListener('mouseup', async (event) => {
-    // event valid?
-    if (typeof event !== 'object' || typeof event.target !== 'object') {
-      return;
-    }
+document.body.addEventListener('mouseup', async (event) => {
+  // event valid?
+  if (typeof event !== 'object' || typeof event.target !== 'object') {
+    return;
+  }
 
-    // don't handle right mouse button
-    if (event.button === 2) {
-      return;
-    }
+  // don't handle right mouse button
+  if (event.button === 2) {
+    return;
+  }
 
-    // only handle left mouse click if ctrl was clicked
-    if (event.button === 0 && !event.ctrlKey) {
-      return;
-    }
+  // only handle left mouse click if ctrl was clicked
+  if (event.button === 0 && !event.ctrlKey) {
+    return;
+  }
 
-    // check for a element with href
-    const aElement = event.target.closest('a');
-    if (aElement === null || typeof aElement !== 'object' || !aElement.href) {
-      return;
-    }
+  // check for a element with href
+  const aElement = event.target.closest('a');
+  if (aElement === null || typeof aElement !== 'object' || !aElement.href) {
+    return;
+  }
 
-    // tell background process to handle the clicked url
-    await browser.runtime.sendMessage({
-      linkClicked: {
-        href: aElement.href,
-        event: {
-          button: event.button,
-          ctrlKey: event.ctrlKey
-        }
+  // tell background process to handle the clicked url
+  await browser.runtime.sendMessage({
+    linkClicked: {
+      href: aElement.href,
+      event: {
+        button: event.button,
+        ctrlKey: event.ctrlKey
       }
-    });
+    }
+  });
 
-  }, false);
-});
+}, false);
