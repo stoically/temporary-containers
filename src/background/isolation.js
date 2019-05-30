2dcc0e578b3c0d999726bda9ab93c5c26fca666a
stoically <stoically@protonmail.com>
2019-05-30 12:33:43 +0200

    Catch invalid regexps (#262)

--- src/background/isolation.js
+++ src/background/isolation.js
@@ -3,7 +3,11 @@ class Isolation {
   matchDomainPattern(url, domainPattern) {
     if (domainPattern.startsWith('/')) {
       const regexp = domainPattern.match(/^\/(.*)\/([gimsuy]+)?$/);
-      return (new RegExp(regexp[1], regexp[2])).test(url);
+      try {
+        return (new RegExp(regexp[1], regexp[2])).test(url);
+      } catch(error) {
+        return false;
+      }
     } else {
       const parsedUrl = new URL(url);
       return parsedUrl.hostname === domainPattern ||
