2e6b7b52a4a33d66b60d6738d7c402c8892f0f41
stoically <stoically@protonmail.com>
2019-07-03 21:07:26 +0200

    refactor(ui): wording

--- src/ui/components/isolation/helper/intro-hint.vue
+++ src/ui/components/isolation/helper/intro-hint.vue
@@ -35,10 +35,8 @@ export default {
           A Navigation or Mouse Click matching
           Configurations other than Never result in Isolation,
           which cancels the Navigation and opens the Target Domain
-          in a new Temporary Container.
-        </div>
-        <div style="margin-top: 15px">
-          Exclude Configurations prevent Isolation if they match.
+          in a new Temporary Container. Exclude Configurations prevent
+          Isolation if they match.
         </div>
         <div style="margin-top: 15px">
           You can click on "Isolation Glossary" (at the bottom of the page)
